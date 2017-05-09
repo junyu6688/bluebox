e22fe3a4d6dea2329fe90a6313d19e55840a33c3
aimeidefashi <295415784@qq.com>
2017-05-09 19:12:33 +0800

    商品添加部分完成
    商品添加部分完成

--- application/Admin/Controller/GoodsController.class.php
+++ application/Admin/Controller/GoodsController.class.php
@@ -10,21 +10,28 @@ class GoodsController extends AdminbaseController {
 	function _initialize() {
 		parent::_initialize();
 		$this->goods_model =D("Common/Goods");
+		$this->goods_type_model =D("Common/Goods_type");
 	}
 	
 	// 后台商品管理列表
 	public function index(){
-		$this->_lists(array("post_status"=>array('neq',3)));
-		
-		$this->display();
+		$count = $this->goods_model->count();
+		$page = $this->page($count, 20);
+		$goods_list = $this ->goods_model
+							->alias('g')
+							->join('left join bb_goods_type gt on g.goods_type=gt.id')
+							->field('g.*,gt.type_name')
+							->order('g.status desc')
+							->limit($page->firstRow , $page->listRows)
+						    ->select();
+		$this->assign("goods_list",$goods_list);
+		$this ->display();
 	}
 	
 	// 添加商品
 	public function add(){
-		$goods_list = $this->goods_model->select();
 		$goods_id = I("get.goods_id",0,'intval');
 		$goods_info=$this->goods_model->where(array('goods_id'=>$goods_id))->find();
-		$this->assign("goods_list",$goods_list);
 		$this->assign("goods_info",$goods_info);
 		$this->display();
 	}
@@ -32,7 +39,16 @@ class GoodsController extends AdminbaseController {
 	// 商品添加提交
 	public function add_post(){
 		if (IS_POST) {
+			if(!empty($_POST['photos_alt']) && !empty($_POST['photos_url'])){
+				foreach ($_POST['photos_url'] as $key=>$url){
+					$photourl=sp_asset_relative_url($url);
+					$_POST['smeta']['photo'][]=array("url"=>$photourl,"alt"=>$_POST['photos_alt'][$key]);
+				}
+			}
+			$_POST['smeta']['thumb'] = sp_asset_relative_url($_POST['smeta']['thumb']);
 			$goods_info=I("post.post");
+			$goods_info['goods_price']=bb_yuan_to_fen($goods_info['goods_price']);
+			$goods_info['smeta']=json_encode($_POST['smeta']);
 			$result=$this->goods_model->add_data($goods_info);
 			if ($result) {
 				$this->success("添加成功！");
@@ -46,15 +62,80 @@ class GoodsController extends AdminbaseController {
 	public function edit(){
 		$goods_id = I("get.goods_id",0,'intval');
 		$goods_info=$this->goods_model->get_info_by_id($goods_id);
+		$goods_info['goods_price']=bb_fen_to_yuan($goods_info['goods_price']);
 		$this->assign("goods_info",$goods_info);
+		$this->assign("smeta",json_decode($goods_info['smeta'],true));
 		$this->display();
 	}
 	
 	// 商品编辑提交
 	public function edit_post(){
 		if (IS_POST) {
-			$goods_info=I("post.post");
-			$result=$this->goods_model->save_data($goods_info);
+			if(!empty($_POST['photos_alt']) && !empty($_POST['photos_url'])){
+				foreach ($_POST['photos_url'] as $key=>$url){
+					$photourl=sp_asset_relative_url($url);
+					$_POST['smeta']['photo'][]=array("url"=>$photourl,"alt"=>$_POST['photos_alt'][$key]);
+				}
+			}
+			$_POST['smeta']['thumb'] = sp_asset_relative_url($_POST['smeta']['thumb']);
+			$goods_info = I("post.post");
+			$goods_info['smeta'] = json_encode($_POST['smeta']);
+			$goods_info['goods_price']=bb_yuan_to_fen($goods_info['goods_price']);
+			$result = $this->goods_model->save_data($goods_info);
+			if ($result) {
+				$this->success("编辑成功！");
+			} else {
+				$this->error("编辑失败！");
+			}
+			 
+		}
+	}
+	public function delete_goods () {
+		$status = I('get.status', 0);
+		$goods_id = I('get.goods_id');
+		$result = $this->goods_model->change_status($goods_id, $status);
+		if ($result) {
+				$this->success("操作成功！");
+			} else {
+				$this->error("操作失败！");
+			}
+	}
+
+	public function index_type(){
+		$goods_list = $this ->get_goods_type_list();
+		$this->assign("goods_list",$goods_list);
+		$this ->display();
+	}
+
+	public function add_type(){
+		$this->display();
+	}
+	
+
+	public function add_type_post(){
+		if (IS_POST) {
+			$type_info=I("post.post");
+			$result=$this->goods_type_model->add_data($type_info);
+			if ($result) {
+				$this->success("添加成功！");
+			} else {
+				$this->error("添加失败！");
+			}
+			 
+		}
+	}
+
+	public function edit_type(){
+		$type_id = I("get.type_id",0,'intval');
+		$type_info=$this->goods_type_model->get_info_by_id($type_id);
+		$this->assign("type_info",$type_info);
+		$this->display();
+	}
+	
+	public function edit_type_post(){
+		if (IS_POST) {
+			$type_info = I("post.post");
+			$result = $this->goods_type_model->save_data($type_info);
 			if ($result) {
 				$this->success("编辑成功！");
 			} else {
@@ -64,7 +145,4 @@ class GoodsController extends AdminbaseController {
 		}
 	}
 
-	public function _lists ($where=array()) {
-
-	}
 }
--- application/Common/Model/GoodsModel.class.php
+++ application/Common/Model/GoodsModel.class.php
@@ -2,6 +2,10 @@
 namespace Common\Model;
 use Common\Model\CommonModel;
 class GoodsModel extends CommonModel{
+	const SHOW_GOODS=0.;   //显示商品
+	const HIDE_GOODS=1;   //下架商品
+
+	const GOODS_INFO_ID='goods_info_id_';   //单条信息key前缀
 	//自动验证
 	// protected $_validate = array(
 	// 		//array(验证字段,验证规则,错误提示,验证条件,附加规则,验证时间)
@@ -11,8 +15,22 @@ class GoodsModel extends CommonModel{
 	// protected function _before_write(&$data) {
 	// 	parent::_before_write($data);
 	// }
+	public function get_info_by_id ($goods_id) {
+		
+		$redis_key = self::GOODS_INFO_ID . $goods_id;
+		$redis_key = md5($this->get_cache_flag_val() . $redis_key);
+		$res = S($redis_key);
+		if ($res === false) {
+			$condition['id'] = $goods_id;
+			$res = $this->where($condition)->find();
+			if ($res !== false)
+				S($redis_key, $res);
+		}
+		return $res;
+	}
+
 	/*
-	 * 增加记录
+	 * 增加商品记录
 	 */
 	public function add_data($data)
 	{
@@ -23,13 +41,36 @@ class GoodsModel extends CommonModel{
 	/*
 	 * 更新商品信息
 	*/
-	public function save_data($goods_id, $data)
+	public function save_data($data)
 	{
-		$condition['id'] = $goods_id;
-		$data['update_time'] = time();
+		$redis_key = self::GOODS_INFO_ID . $data['id'];
+		$redis_key = md5($this->get_cache_flag_val() . $redis_key);
+		$condition['id'] = $data['id'];
 		$res = $this->where($condition)->save($data);
-
+		if ($res !== false) {
+			S($redis_key, null);
+		}
 		return $res;
 
 	}
+	/*
+	 * 改编商品状态
+	*/
+	public function change_status($goods_id, $status)
+	{
+		$redis_key = self::GOODS_INFO_ID . $goods_id;
+		$redis_key = md5($this->get_cache_flag_val() . $redis_key);
+		if($status != 0) 
+			$status = 0;
+		else
+			$status = 1;
+		$condition['id'] = $goods_id;
+		$data = array('id'=>$goods_id, 'status'=>$status); 
+		$res = $this->where($condition)->save($data);
+		if ($res !== false) {
+			S($redis_key, null);
+		}
+		return $res;
+	}
+
 }
